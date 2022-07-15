d6126a1df76cb112c4481d7086324bd5831dbd5c
ValenLive <phan.star2016@gmail.com>
2022-07-15 20:50:07 +0300

    Update Main.java

--- Challenge_1/src/Main.java
+++ Challenge_1/src/Main.java
@@ -52,7 +52,7 @@ public class Main {
 
     public static int getFactorial(int n) {
         int fact = 1;
-        for (int i = 2; i <= n; i++) {
+        for (int i = 1; i < n; i++) {
             fact *= i;
         }
         return fact;
