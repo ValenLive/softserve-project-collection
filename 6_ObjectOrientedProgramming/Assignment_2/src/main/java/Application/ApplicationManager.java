a3c9853ba8dbe35af92761125b0ec819598b51e3
ValenLive <phan.star2016@gmail.com>
2022-08-05 19:40:32 +0300

    update

--- 6_ObjectOrientedProgramming/Assignment_2/src/main/java/Application/ApplicationManager.java
+++ 6_ObjectOrientedProgramming/Assignment_2/src/main/java/Application/ApplicationManager.java
@@ -6,11 +6,16 @@ import java.util.stream.Collectors;
 
 public class ApplicationManager {
     public void executeApplication() {
+        //Getting list of employees
         List<Employee> employees = getEmployeeList();
+
+        //Calculating salary
         calculateSalary(employees);
+
+        //Printing salary
         printEmployeesList(employees);
 
-
+        //Sorting list of employees in descending order
         System.out.println("--- Sorted in descending order: ---");
         printEmployeesList(sortEmployeeList(employees));
 
@@ -19,8 +24,8 @@ public class ApplicationManager {
     private List<Employee> getEmployeeList() {
         return List.of(
                 new SalariedEmployee("0345", "Thomas", "543_645", 3.5, 11120),
-                new SalariedEmployee("1237", "Abobus", "6343_645", 5.5, 200),
-                new ContractEmployee("5643", "Bitcher", "23421556", 12500),
+                new SalariedEmployee("1237", "Max", "6343_645", 5.5, 200),
+                new ContractEmployee("5643", "Emily", "23421556", 12500),
                 new ContractEmployee("0112", "Jonah", "390943345", 22500)
         );
     }
