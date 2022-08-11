a779f4d2c9dd95c0db9e63e8888f06ccee40126a
ValenLive <phan.star2016@gmail.com>
2022-08-11 21:41:19 +0300

    Task update

--- 7_StringRegularExpressions/Assignment/src/main/java/Application/TaskCollection.java
+++ 7_StringRegularExpressions/Assignment/src/main/java/Application/TaskCollection.java
@@ -2,33 +2,34 @@ package Application;
 
 import org.apache.commons.lang3.tuple.ImmutablePair;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
 
 public class TaskCollection extends TaskSolver {
     public void executePracticalTasks() {
+        System.out.println("-----Task1-----");
         executeTask1();
-        System.out.println("--------------");
+        System.out.println("-----Task2-----");
         executeTask2();
-        System.out.println("--------------");
+        System.out.println("-----Task3-----");
         executeTask3();
-        System.out.println("--------------");
     }
 
     public void executeHomework() {
+        System.out.println("-----Homework1-----");
         executeHomework1();
-        System.out.println("--------------");
+        System.out.println("-----Homework2-----");
         executeHomework2();
-        System.out.println("--------------");
+        System.out.println("-----Homework3-----");
         executeHomework3();
-        System.out.println("--------------");
     }
-
 
     /**
      * Enter the two variables of type String. Determine whether the first variable substring second. For
      * example, if you typed «IT» and «IT Academy» you must receive true.
      */
+
     private void executeTask1() {
         boolean hasSubstring = isSubstring(new ImmutablePair<>(Input.readStringInput(), Input.readStringInput()));
         System.out.println(hasSubstring);
@@ -40,9 +41,10 @@ public class TaskCollection extends TaskSolver {
      * name;
      * name, middle name and last name
      */
+
     private void executeTask2() {
         String user = getSurnameAndInitials("Valentyn Stavruk Romanovych");
-        System.out.println(user);
+        printString(user);
     }
 
     /**
@@ -50,22 +52,54 @@ public class TaskCollection extends TaskSolver {
      * expressions implement checking the username for validity. Input five names in the main method . Output a
      * message to the console of the validation of each of the entered names.
      */
+
     private void executeTask3() {
         List<String> usernameList = List.of("Xx_Gamer_xX", "Max", "Heisenberg", "''''''", " ", "1", "a", "88_", "SuperDuperLongLongWord");
         printValidStrings(usernameList, Pattern.compile("\\w{2,15}"));
     }
 
+    /*
+     * =============================HOME WORK=================================
+     */
+
+    /**
+     * Enter in console sentence of five words.
+     * <p>
+     * display the longest word in the sentence
+     * determine the number of its letters
+     * bring the second word in reverse order
+     */
 
     private void executeHomework1() {
-
+        List<String> sentences = Arrays.asList(Input.readStringInput().split(" "));
+        String longestWord = getLongestWord(sentences);
+        printString(longestWord);
+        printStringLength(longestWord);
+        printSecondWordOfList(sentences);
     }
+
+    /**
+     * Enter a sentence that contains the words between more than one space. Convert all spaces,
+     * consecutive, one. For example, if we introduce the sentence "I am learning Java Core», we
+     * have to get the "I'm learning Java Core»
+     */
 
     private void executeHomework2() {
-
+        printRefactoredWhitespaceText(Input.readStringInput());
     }
 
-    private void executeHomework3() {
+    /**
+     * Implement a pattern for US currency: the first symbol "$", then any number of digits, dot and two
+     * digits after the dot. Enter the text from the console that contains several occurrences of US currency.
+     * Display all occurrences on the screen.
+     */
 
+    public void executeHomework3() {
+        String str = Input.readStringInput();
+        System.out.println(
+                Pattern.compile("^\\$?(\\d{1,3},(\\d{3},)*\\d{3}|\\d+)(.\\d\\d)?$")
+                        .matcher(str).matches() ? "$" + str : "Invalid number"
+        );
     }
 
 
--- 7_StringRegularExpressions/Assignment/src/main/java/Application/TaskSolver.java
+++ 7_StringRegularExpressions/Assignment/src/main/java/Application/TaskSolver.java
@@ -2,7 +2,9 @@ package Application;
 
 import org.apache.commons.lang3.tuple.ImmutablePair;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 import java.util.regex.Pattern;
 
 abstract class TaskSolver {
@@ -27,5 +29,28 @@ abstract class TaskSolver {
                         .matcher(item)
                         .matches())
                 .forEach(System.out::println);
+    }
+
+    public String getLongestWord(List<String> list) {
+        return list.stream()
+                .max(Comparator.comparingInt(String::length))
+                .map(Objects::toString)
+                .orElseThrow(RuntimeException::new);
+    }
+
+    public void printString(String str) {
+        System.out.println(str);
+    }
+
+    public void printStringLength(String str) {
+        System.out.println("String length: " + str.length());
+    }
+
+    public void printSecondWordOfList(List<String> sentences) {
+        System.out.println("Second word: " + (sentences.contains(sentences.get(1)) ? sentences.get(1) : new ArrayIndexOutOfBoundsException()));
+    }
+
+    public void printRefactoredWhitespaceText(String str) {
+        System.out.println(str.replaceAll("\\s+", " "));
     }
 }
