a4bf38f5658b7c039da101848538b3cd01be179e
ValenLive <phan.star2016@gmail.com>
2022-08-13 17:21:33 +0300

    Update Application.java

--- 8_Collections/Homework/src/main/java/Application/Application.java
+++ 8_Collections/Homework/src/main/java/Application/Application.java
@@ -42,6 +42,13 @@ public class Application extends HomeworkSolver implements Homework {
         System.out.println(sortedList);
     }
 
+    /**
+     * In the main() method declare map employeeMap of pairs <Integer, String>.
+     * Add to employeeMap seven pairs (ID, name) of some persons. Display the map on the screen.
+     * Ask user to enter ID, then find and write corresponding name from your map. If you can't find this ID - say about it to user (use function containsKey()).
+     * Ask user to enter name, verify than you have a name in your map and write corresponding ID. If you can't find this name - say about it to user (use function containsValue()).
+     */
+
     @Override
     public void executeHomework2() {
 
